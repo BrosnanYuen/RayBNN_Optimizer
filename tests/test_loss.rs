dfdc069e6080db1239815874158d269d00a526ec
brosnanyuen <brosnany@uvic.ca>
2023-10-18 21:50:38 -0700

    save

--- tests/test_loss.rs
+++ tests/test_loss.rs
@@ -21,4 +21,43 @@ fn test_loss() {
 
 
 
+    let Y_cpu: Vec<f64> = vec![0.0,  1.0,  0.0,  0.0,  0.0,  0.0,  0.0           ,0.0,  0.0,  0.0,  0.0,  0.0,  1.0,  0.0           ,0.0,  0.0,  0.0,  1.0,  0.0,  0.0,  0.0 ];
+    let Y = arrayfire::Array::new(&Y_cpu, arrayfire::Dim4::new(&[7, 3, 1, 1]));
+
+
+    let Yhat_cpu: Vec<f64> = vec![0.8,  0.3,  0.5,  0.2,  0.4,  0.1,  0.7        ,0.1,  0.2,  0.4,  0.2,  0.1,  0.6,  0.3           ,0.9,  0.2,  0.4,  0.2,  0.5,  0.6,  0.3 ];
+    let Yhat = arrayfire::Array::new(&Yhat_cpu, arrayfire::Dim4::new(&[7, 3, 1, 1]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    let mut cross_entropy_grad = RayBNN_Optimizer::Continuous::Loss::softmax_cross_entropy_grad(&Yhat,&Y);
+
+
+    let mut cross_entropy_grad_act_cpu:Vec<f64> = vec![0.067097, -0.292637, 0.049707, 0.036824, 0.044977, 0.03332, 0.060712, 0.03954, 0.043698, 0.053373, 0.043698, 0.03954, -0.268143, 0.048294, 0.073105, 0.036303, 0.04434, -0.29703, 0.049004, 0.054158, 0.040121];
+    let mut cross_entropy_grad_cpu = vec!(f64::default();cross_entropy_grad.elements());
+
+
+    cross_entropy_grad.host(&mut cross_entropy_grad_cpu);
+
+    cross_entropy_grad_cpu = cross_entropy_grad_cpu.par_iter().map(|x|  (x * 1000000.0).round() / 1000000.0 ).collect::<Vec<f64>>();
+
+    cross_entropy_grad_act_cpu = cross_entropy_grad_act_cpu.par_iter().map(|x|  (x * 1000000.0).round() / 1000000.0 ).collect::<Vec<f64>>();
+
+
+
+    assert_eq!(cross_entropy_grad_act_cpu, cross_entropy_grad_cpu);
+
+
 }
