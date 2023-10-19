cd7987deaa8e355b45eeaccff933fe5a05fd7f01
brosnanyuen <brosnany@uvic.ca>
2023-10-18 21:11:07 -0700

    save

--- src/Continuous/Loss.rs
+++ src/Continuous/Loss.rs
@@ -1,37 +1,36 @@
 use arrayfire;
 
 
+const TWO_F64: f64 = 2.0;
+const ONE_F64: f64 = 1.0;
+const ZERO_F64: f64 = 0.0;
 
-pub fn softmax_cross_entropy<Z: arrayfire::FloatingPoint>(
-	yhat: &arrayfire::Array<Z>,
-	y: &arrayfire::Array<Z>) -> Z {
+
+
+
+
+
+
+
+
+pub fn softmax_cross_entropy_grad(
+	yhat: &arrayfire::Array<f64>,
+	y: &arrayfire::Array<f64>) -> arrayfire::Array<f64> {
 		let output_size = y.dims()[0];
 		let batch_size = y.dims()[1];
 		let batch_size_f64 = batch_size as f64;
 
-		let mut yhatmax = arrayfire::max(yhat,0);
-		yhatmax = arrayfire::transpose(&yhatmax, false);
+
+		let mut expyhat = arrayfire::exp(yhat);
+		expyhat = arrayfire::clamp(&expyhat, &zero, &high, false);
+
+		let mut sumyhat = arrayfire::sum(&expyhat,0);
+		sumyhat = arrayfire::clamp(&sumyhat, &zero, &high, false);
+
+		expyhat = arrayfire::div(&expyhat,&sumyhat, true);
 
 
-		let (_,mut yidx) = arrayfire::imax(y,0);
-		yidx = arrayfire::transpose(&yidx, false);
-
-
-		let mut actmax = arrayfire::flat(yhat);
-
-		let N_dims = arrayfire::Dim4::new(&[batch_size,1,1,1]);
-		let repeat_dims = arrayfire::Dim4::new(&[1,1,1,1]);
-		let offset = (output_size as u32)*arrayfire::iota::<u32>(N_dims,repeat_dims);
-
-		yidx = yidx + offset;
-		actmax = arrayfire::lookup(&actmax, &yidx, 0);
-
-
-		let diff = yhatmax -  actmax;
-
-		let (r0,_) = arrayfire::sum_all::<f64>(&diff);
-
-		(one/batch_size_f64)*( r0 )
+		(one/batch_size_f64)*( expyhat - y )
 }
 
 
