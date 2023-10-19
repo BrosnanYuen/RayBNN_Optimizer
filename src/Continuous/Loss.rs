ab6151efd15a590b77fd051c7140efbf5e0e952d
brosnanyuen <brosnany@uvic.ca>
2023-10-18 21:15:11 -0700

    save

--- src/Continuous/Loss.rs
+++ src/Continuous/Loss.rs
@@ -13,9 +13,9 @@ const ZERO_F64: f64 = 0.0;
 
 
 
-pub fn softmax_cross_entropy_grad(
-	yhat: &arrayfire::Array<f64>,
-	y: &arrayfire::Array<f64>) -> arrayfire::Array<f64> {
+pub fn softmax_cross_entropy_grad<Z: arrayfire::FloatingPoint  >(
+	yhat: &arrayfire::Array<Z>,
+	y: &arrayfire::Array<Z>) -> arrayfire::Array<Z> {
 
 
 		let single_dims = arrayfire::Dim4::new(&[1,1,1,1]);
