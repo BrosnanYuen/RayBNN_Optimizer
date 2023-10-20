02b61d91e917d190efce6a7d95d1926f88778b2a
brosnanyuen <brosnany@uvic.ca>
2023-10-19 18:30:13 -0700

    save

--- src/Continuous/GD.rs
+++ src/Continuous/GD.rs
@@ -8,6 +8,25 @@ const ZERO_F64: f64 = 0.0;
 
 
 
+pub fn adam(
+	beta0: f64
+	,beta1: f64
+	,direction: &mut arrayfire::Array<f64>
+	,mt: &mut arrayfire::Array<f64>
+	,vt: &mut arrayfire::Array<f64>)
+	{
+
+		*mt = (mt.clone())*beta0  + (one-beta0)*(direction.clone());
+		*vt =  (vt.clone())*beta1  + (one-beta1)*arrayfire::pow(direction,&two,false);
+
+		let nmt = mt.clone()/(one-beta0);
+		let mut nvt = vt.clone()/(one-beta1);
+		nvt = arrayfire::sqrt(&nvt) + epsilon;
+
+		*direction = (nmt/nvt);
+}
+
+
 
 
 
