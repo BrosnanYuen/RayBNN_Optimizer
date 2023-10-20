8162671f345b66ec75f54f3089be25bde8e52364
brosnanyuen <brosnany@uvic.ca>
2023-10-19 18:08:43 -0700

    save

--- src/Continuous/GD.rs
+++ src/Continuous/GD.rs
@@ -16,6 +16,13 @@ pub fn momentum(
 	,grad: &arrayfire::Array<f64>
 	,dir: &mut arrayfire::Array<f64>)
 	{
+
+		let single_dims = arrayfire::Dim4::new(&[1,1,1,1]);
+
+		let ONE = arrayfire::constant::<f64>(ONE_F64,single_dims).cast::<Z>();
+	
+
+
 		*dir = (dir.clone()*beta)  + (one-beta)*(grad.clone());
 }
 
