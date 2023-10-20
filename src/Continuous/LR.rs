01d2502c4298fc7cb0309e8dd4660bf6de7658f2
brosnanyuen <brosnany@uvic.ca>
2023-10-20 14:47:35 -0700

    save

--- src/Continuous/LR.rs
+++ src/Continuous/LR.rs
@@ -11,39 +11,6 @@ use arrayfire;
 
 
 
-pub fn BTLS<Z: arrayfire::FloatingPoint>(
-	loss: impl Fn(&arrayfire::Array<Z>) -> f64
-	,loss_grad: impl Fn(&arrayfire::Array<Z>) -> arrayfire::Array<Z>
-	,init_point: &arrayfire::Array<Z>
-	,direction: &arrayfire::Array<Z>
-	,gamma: f64
-	,rho: f64
-	,alpha_max: f64) -> f64
-	{
-		let mut alpha: f64 = alpha_max;
-		let init_loss = loss(init_point);
-
-		let mut next_point = init_point.clone() + (alpha)*direction.clone();
-		let mut f0  = loss(&next_point);
-
-		let init_grad = loss_grad(init_point);
-		let v0 = rho*(arrayfire::mul(direction, &init_grad, false));
-		let (t0,t1) = arrayfire::sum_all(&v0);
-		let mut f1  = init_loss.clone() + (alpha)*t0;
-
-		while (f0 > f1)
-		{
-			alpha = (alpha)*gamma;
-			next_point = init_point.clone() + (alpha)*direction.clone();
-			f0  = loss(&next_point);
-			f1  = init_loss.clone() + (alpha)*t0;
-		}
-		alpha
-}
-
-
-
-
 
 
 
