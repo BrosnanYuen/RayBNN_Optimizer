e45584762ae86d0b40a6bd416c0169307b137d81
brosnanyuen <brosnany@uvic.ca>
2023-10-22 18:35:44 -0700

    save

--- tests/test_classification.rs
+++ tests/test_classification.rs
@@ -44,4 +44,42 @@ fn test_classification() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    let y_cpu: Vec<u32> = vec![2,1,3,1,3,1,0,3,2,2,0,0,1,3,3,1];
+    let y = arrayfire::Array::new(&y_cpu, arrayfire::Dim4::new(&[y_cpu.len() as u64, 1, 1, 1]));
+
+    let yhat_cpu: Vec<u32> = vec![2,3,3,1,2,1,0,3,0,2,0,0,1,2,2,3];
+    let yhat = arrayfire::Array::new(&yhat_cpu, arrayfire::Dim4::new(&[yhat_cpu.len() as u64, 1, 1, 1]));
+
+    let confusion = RayBNN_Optimizer::Discrete::Classification::confusion_matrix(&yhat,&y,4);
+
+    let mut confusion_cpu = vec!(u32::default();confusion.elements());
+
+
+    confusion.host(&mut confusion_cpu);
+
+
+    let mut confusion_act:Vec<u32> = vec![3, 0, 1, 0, 0, 3, 0, 0, 0, 0, 2, 3, 0, 2, 0, 2];
+
+
+    assert_eq!(confusion_act, confusion_cpu);
+
+
+
+
+
+
+
 }
